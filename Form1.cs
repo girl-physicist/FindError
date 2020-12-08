7ddc0676a66b2a63538daf7c03fbd697c2ea7395
Анастасия Роткевич <girl-physicist@live.ru>
2020-12-08 10:42:36 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (FindErr())
+            {
+                textBox2.Text = "No Error";
+            }
+        }
+        private bool FindErr()
+        {
+            try
+            {
+                int a = Int32.Parse(textBox1.Text);
+                return true;
+            }
+            catch 
+            {
+                MessageBox.Show("Error");
+                return false;
+            }
+        }
+    }
+}
